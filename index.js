dcaa7cf92ef6a943ddf6905df9237d56abdb3399
Suraj Jha <getsurajjha@gmail.com>
2024-04-08 00:42:53 +0545

    added response

--- index.js
+++ index.js
@@ -42,6 +42,89 @@ const program =  new Program(idl, programID, provider);
 
 const purchases = await program.account.purchase.all();
 
+const htmlContent = `
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- https://i.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+      rel="stylesheet"
+    />
+    <title>Purchased</title>
+    <style>
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+
+      .wrapper {
+        max-width: 1400px;
+        margin: 1rem auto;
+        text-align: center;
+        border: 4px solid #21c179;
+        border-radius: 30px;
+        padding: 3rem 0;
+      }
+      .payment_successful {
+        font-size: 26px;
+        padding: 1rem 0;
+        font-family: "Roboto Mono";
+      }
+      .title {
+        font-size: 48px;
+        font-weight: 600;
+        padding: 1rem;
+        font-family: "Roboto Mono";
+      }
+      .highlight {
+        color: #2e47b6;
+      }
+      .sub_title {
+        font-size: 24px;
+        padding: 1rem 0;
+        font-weight: 500;
+        font-family: "Roboto Mono";
+      }
+      .success_icon {
+        fill: #21c179;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="wrapper">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        height="79"
+        viewBox="0 0 24 24"
+        width="79"
+        class="success_icon"
+      >
+        <circle cx="12" cy="12" fill="#21c179" r="10" />
+        <path
+          clip-rule="evenodd"
+          d="m16.6766 8.58327c.1936.19698.1908.51355-.0062.70708l-5.7054 5.60545c-.1914.1881-.4972.1915-.6927.0078l-2.67382-2.5115c-.20128-.189-.21118-.5055-.02212-.7067.18906-.2013.50548-.2112.70676-.0222l2.32368 2.1827 5.3628-5.26888c.1969-.19353.5135-.19073.707.00625z"
+          fill="#fff"
+          fill-rule="evenodd"
+        />
+      </svg>
+      <h3 class="payment_successful">Payment has been received for $20</h3>
+      <h1 class="title">
+        Your <span class="highlight">NEBULA</span> subscription was successful!
+      </h1>
+
+      <p class="sub_title">Thank you for your purchase.</p>
+    </div>
+  </body>
+</html>
+`
+
 app.get('/', async (req, res) => {
     if (!req.query.purchaseAddress || !req.query.bump) {
         res.send('Please provide purchaseAddress');
@@ -82,7 +165,7 @@ app.get('/', async (req, res) => {
         console.log(error)
         res.status(500).send("Something went horribly wrong")
     }
-    res.send('Successful response.');
+    res.send(htmlContent);
     return;
 })
 
